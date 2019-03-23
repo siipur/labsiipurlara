98071111bdb9fe734e198c2292407b9ad83f84fa
siipur <ghopurnomo@gmail.com>
2019-03-23 18:00:50 +0700

    edit ArticleFactory untuk proses seeding ke database

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->text(50),
+        'body' => $faker->text(300),
     ];
 });
