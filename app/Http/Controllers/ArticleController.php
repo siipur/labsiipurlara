ec7edbd9a27e8c1885b5d2e71f2a52e80d73be17
siipur <ghopurnomo@gmail.com>
2019-03-23 19:22:06 +0700

    edit ArticleController untuk seeting penggunaan API resources

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
