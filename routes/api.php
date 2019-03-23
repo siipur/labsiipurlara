4631b4bf8ef3a7990a24b16bb7da9e246c7d4789
siipur <ghopurnomo@gmail.com>
2019-03-23 19:03:53 +0700

    edit routes api untuk articles

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+## ROUTES untuk Users ##
 // List users
 Route::get('users', 'UserController@index');
-
 // List single Users
 Route::get('users/{id}', 'UserController@show');
-
 // Create new Users
 Route::post('users', 'UserController@store');
-
 // Update Users
 Route::put('users', 'UserController@store');
-
 // Delete Users
 Route::delete('users/{id}', 'UserController@destroy');
+
+//## ROUTES untuk Articles ##//
+# List articles
+Route::get('articles', 'ArticleController@index');
+# List single article
+Route::get('articles/{id}', 'ArticleController@show');
+# Create new article
+Route::post('articles', 'ArticleController@store');
+# Update article
+Route::put('articles', 'ArticleController@store');
+# Delete article
+Route::delete('articles/{id}', 'ArticleController@destroy');
